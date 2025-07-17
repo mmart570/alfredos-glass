1ef4b0ed32b489f519295a4c63916c540afc0636
Matthew Martinez <mmart570@ucr.edu>
2025-07-16 17:28:22 -0700

    made rough our story component

--- src/components/home/OurStory.tsx
+++ src/components/home/OurStory.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import Image from "next/image";
 
 const OurStory = () => {
   return (
-    <div className="text-6xl text-[#172E73] flex justify-center font-semibold py-36">
-      OUR STORY
+    <div className="flex w-full flex-col items-center justify-center bg-gray-300 pb-40">
+      <h1 className="text-6xl text-[#172E73] flex justify-center font-semibold py-36">
+        OUR STORY
+      </h1>
+      <div className="flex flex-col md:flex-row items-center md:items-start">
+        <Image
+          src="/glass.jpg"
+          alt="Papa"
+          width="1000"
+          height="200"
+          className="w-full ml-8 shadow-xl"
+          priority
+        />
+        <p className="ml-20 mt-20 mr-8 text-4xl text-[#172E73] font-semibold">
+          We have serviced San Bernardino and cities across the Inland Empire
+          for over 10 years. From sudden business break-ins to the most worn
+          down windows, we aim to give our customers top-notch glass repair
+          solutions with a personalized touch.
+        </p>
+      </div>
     </div>
   );
 };
