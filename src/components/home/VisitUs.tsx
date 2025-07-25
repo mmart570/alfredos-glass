13d6b9dddcaa2928ca84c91bb5d145e0faf363b3
Matthew Martinez <mmart570@ucr.edu>
2025-07-24 22:32:48 -0700

    adding google maps to home page ...

--- src/components/home/VisitUs.tsx
+++ src/components/home/VisitUs.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
 
 const VisitUs = () => {
-  return (
-    <div className="text-6xl text-[#172E73] flex justify-center font-semibold py-36">
-      VISIT US
-    </div>
-  );
+  const mapRef = React.useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const initMap = async () => {
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+        version: "weekly",
+      });
+
+      const { Map } = await loader.importLibrary("maps");
+
+      const position = {
+        lat: 34.116508985936534,
+        lng: -117.26263771794464,
+      };
+
+      // map options
+      const mapOptions: google.maps.MapOptions = {
+        center: position,
+        zoom: 17,
+        mapId: "MY_NEXTJS_MAPID",
+      };
+
+      //setup map
+      const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+    };
+
+    initMap();
+  }, []);
+
+  return <div style={{ height: "600px" }} ref={mapRef} />;
 };
 
 export default VisitUs;
