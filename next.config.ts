3e7271806a183296a8e21fe49783be9ec477458d
Matthew Martinez <mmart570@ucr.edu>
2025-08-12 15:31:29 -0700

    resolving conflicts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/your-repo-name",
-  assetPrefix: "/your-repo-name/",
+  basePath: isProd ? "/alfredos-glass" : "",
+  assetPrefix: isProd ? "/alfredos-glass" : "",
   images: {
     unoptimized: true,
   },
