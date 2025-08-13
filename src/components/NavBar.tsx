a3567b6f51f2208e2098a0488ff3edb16261d71f
Matthew Martinez <mmart570@ucr.edu>
2025-08-12 21:53:32 -0700

    finished navbar responsiveness

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,17 @@
+"use client";
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleNav = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="w-full h-40 z-50">
       <div className="flex items-center justify-center h-full w-full px-4 2xl:px-16 text-[#172E73] font-semibold">
@@ -12,7 +21,7 @@ const NavBar = () => {
             alt="Logo"
             width="150"
             height="50"
-            className="cursor-pointer  whileHover: { scale: 1.02 }, transition: {stiffness: 300 },"
+            className="cursor-pointer whileHover: { scale: 1.02 }, transition: {stiffness: 300 },"
             priority
           />
         </Link>
@@ -30,8 +39,62 @@ const NavBar = () => {
             </Link>
           </ul>
         </div>
+        <div onClick={handleNav} className="sm:hidden cursor-pointer pl-24">
+          <AiOutlineMenu size={25} />
+        </div>
       </div>
-      <div className="bg-[#172E73] mb-4 hidden h-0.5 w-full md:flex" />
+      <div className="bg-[#172E73] mb-4 h-0.5 w-full" />
+      <div
+        className={
+          menuOpen
+            ? "fixed z-100 left-0 top-0 w-[65%] sm:hidden h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+            : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+        }
+      >
+        <div className="flex w-full items-center justify-end">
+          <div onClick={handleNav} className="cursor-pointer">
+            <AiOutlineClose size={25} />
+          </div>
+        </div>
+        <div className="flex-col py-10">
+          <ul>
+            <Link href="/">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-10 text-xl cursor-pointer uppercase font-semibold text-[#172E73]"
+              >
+                Home
+              </li>
+            </Link>
+            <Link href="/services">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-10 text-xl cursor-pointer uppercase font-semibold text-[#172E73]"
+              >
+                Services
+              </li>
+            </Link>
+            <Link href="/gallery">
+              <li
+                onClick={() => setMenuOpen(false)}
+                className="py-10 text-xl cursor-pointer uppercase font-semibold text-[#172E73]"
+              >
+                Gallery
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <Link href="/">
+          <Image
+            src="/agslogo.png"
+            alt="Logo"
+            width="205"
+            height="75"
+            className="cursor-pointer pt-6"
+            priority
+          />
+        </Link>
+      </div>
     </nav>
   );
 };
